32838906a89bf499b9fe722654669c31878eb3f9
Kevin Jay <kjay@savingstar.com>
2015-01-25 12:47:30 -0500

    change mountpoint

--- survtest/config/routes.rb
+++ survtest/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount SurveyorGui::Engine => "/my/survey/engine", :as => "surveyor_gui"
+  mount SurveyorGui::Engine => "/surveyor_gui", :as => "surveyor_gui"
   mount Surveyor::Engine => "/surveys", :as => "surveyor"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
